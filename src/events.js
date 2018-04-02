0fad5671560f018a19ff6504a676b794944f3661
jamsinclair <jamsinclairnz@gmail.com>
2018-04-02 12:51:34 +0900

    Add an events constant file

---
+++ src/events.js
@@ -0,0 +1,15 @@
+export const NATIVE_EVENTS = {
+  CHANGE: 'change',
+  CLICK: 'click',
+  DRAG_START: 'dragstart',
+  DRAG_END: 'dragend',
+  DRAG_ENTER: 'dragenter',
+  DRAG_LEAVE: 'dragleave',
+  DRAG_OVER: 'dragover',
+  DROP: 'drop'
+}
+
+export const CUSTOM_EVENTS = {
+  DESTROY: 'destroy',
+  FILES_ADDED: 'files:added'
+}
