a16ee3c826b22bab26c4a4978db9a89fc130c89b
jamsinclair <jamsinclairnz@gmail.com>
2018-04-07 00:18:57 +0900

    Handle Parcel hot module replacement better in example app

--- examples/index.js
+++ examples/index.js
@@ -3,22 +3,23 @@ import Ascender from '../src/ascender'
 const dropAreaNode = document.querySelector('.drop-area')
 const previewNode = document.querySelector('.preview-container')
 
+const instance = new Ascender(dropAreaNode)
 
-if (!window.instance) {
-  window.instance = new Ascender(dropAreaNode)
+instance.on('file:added', (file) => {
+  console.log('Current files: ', instance.files)
 
-  instance.on('file:added', (file) => {
-    console.log('Current files: ', instance.files)
+  file.getDataUri().then(uri => {
+    const newImg = document.createElement('img')
+    newImg.src = uri
+    newImg.classList.add('image-preview')
+    previewNode.appendChild(newImg)
+  }).catch(err => {
+    console.log('Problem showing image', err)
+  })
+})
 
-    file.getDataUri().then(uri => {
-      const newImg = document.createElement('img')
-      newImg.src = uri
-      newImg.classList.add('image-preview')
-      previewNode.appendChild(newImg)
-    }).catch(err => {
-      console.log('problem showing image', err)
-    })
+if (module.hot) {
+  module.hot.dispose(() => {
+    window.location.reload()
   })
 }
-
-
