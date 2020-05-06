1a40640aceef1cf93cc5c16b268e577755dd4a48
jamsinclair <jamsinclairnz@gmail.com>
2020-05-07 07:21:47 +0900

    Add definition file

---
+++ index.d.ts
@@ -0,0 +1,31 @@
+import * as mitt from 'mitt'
+
+enum DropZoneEvents {
+  click = 'click',
+  dragstart = 'dragstart',
+  dragend = 'dragend',
+  dragenter = 'dragenter',
+  dragleave = 'dragleave',
+  dragover = 'dragover',
+  drop = 'drop',
+  fileadded = 'fileadded',
+}
+
+interface DropZoneOptions {
+  accept?: string
+  clickable?: boolean
+  dragClass?: string
+  multiple?: boolean
+}
+
+interface DropZoneInstance {
+  destroy (): void
+  on(type: DropZoneEvents, handler: mitt.Handler): void;
+  on(type: "*", handler: mitt.WildcardHandler): void;
+  off(type: DropZoneEvents, handler: mitt.Handler): void;
+  off(type: "*", handler: mitt.WildcardHandler): void;
+}
+
+export function createBinary(file: File): Promise<ArrayBuffer>
+export function createDataUri(file: File): Promise<string>
+export function DropZone(element: Element, options?: DropZoneOptions): DropZoneInstance
