9bb9452760ebe281b639cf067d26acb26c2add2b
jamsinclair <jamsinclairnz@gmail.com>
2020-05-21 23:55:57 +0900

    Support bundling dependencies in umd bundle with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,47 @@
 import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
+
+const bundleExternal = process.env.BABEL_ENV === 'umd'
+
+const globals = {
+  mitt: 'mitt',
+  '@babel/runtime/helpers/defineProperty': '_defineProperty'
+}
+const external = ['mitt', '@babel/runtime/helpers/defineProperty']
+
+const cjs = {
+  file: './dist/cjs/index.js',
+  format: 'cjs',
+  globals
+}
+
+const esm = {
+  file: './dist/esm/index.js',
+  format: 'esm',
+  globals
+}
+
+const umd = {
+  file: './dist/umd/index.js',
+  format: 'umd',
+  name: 'Ascender',
+  globals
+}
+
+const commonjsPlugin = commonjs()
+const resolvePlugin = resolve({ extensions: ['.ts'] })
+const babelPlugin = babel({
+  babelHelpers: bundleExternal ? 'bundled' : 'runtime',
+  extensions: ['.ts'],
+  plugins: bundleExternal ? [] : undefined
+})
 
 export default {
   input: './src/index.ts',
-  external: [ 'mitt', '@babel/runtime' ],
-  output: [{
-    file: './dist/cjs/index.js',
-    format: 'cjs',
-  }, {
-    file: './dist/esm/index.js',
-    format: 'esm',
-  }, {
-    file: './dist/umd/index.js',
-    format: 'umd',
-    name: 'Ascender'
-  }],
-  plugins: [
-    resolve({ extensions: ['.ts'] }),
-    babel({
-      babelHelpers: 'runtime',
-      extensions: ['.ts']
-    })
-  ]
+  external: bundleExternal ? undefined : external,
+  output: bundleExternal ? [umd] : [cjs, esm],
+  plugins: bundleExternal
+    ? [resolvePlugin, commonjsPlugin, babelPlugin]
+    : [resolvePlugin, babelPlugin]
 }
